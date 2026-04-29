3e60b11c37e98f98405d9c034086c63f60dc1804
evalstate <1936278+evalstate@users.noreply.github.com>
2026-04-29 21:41:51 +0100

    Apply PR #41827 FlashAttention compile guard

--- src/transformers/modeling_flash_attention_utils.py
+++ src/transformers/modeling_flash_attention_utils.py
@@ -517,7 +517,7 @@ def _is_packed_sequence(position_ids, batch_size):
         2. Flattened sequences only are supported
         3. Compile-friendly `not (torch.diff(position_ids, dim=-1) >= 0).all()`, i.e. we have multiple increasing sequences
     """
-    if position_ids is None:
+    if is_tracing(position_ids) or position_ids is None:
         return False
 
     increasing_position_sequences = (
